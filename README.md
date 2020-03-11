01b9e698e105eb8080a04efdfb823c47bb020d91
Qiyang Hu <huqy@Qiyang-Hus-MacBook-Pro.local>
2020-03-11 12:52:04 -0700

    Update for Artifact Description Appendix

--- README.md
+++ README.md
@@ -1,7 +1,45 @@
-This gitlab repo contains the source code for the python codes to process module files. The contents in the repository include:
+# Module File Processing
 
- - The example module files generated from Spack and the module files in TACC and Comet.
- - The python script `gen_json.py` to generate the JSON file template from Spack-generated module files.
- - The python script `modulefilter_h_03.py` to import Spack-generated module files and generate the sqlite database and process/export to the new module path directory.
- - Examples of JSON and database files.
- - Some misc files.
+This gitlab repo contains the source code for the python codes to process module files. 
+
+## Requirements
+Python 3.5 or later is needed to run the script `modulefilter.py`
+
+A deployment of a group of software packaged has to be built through Spack. The Spack's module file configuration has to enable both `tcl` and `lmod` block in its `modules.yaml` file. A simple example of configuration can be checked in Spack's [module tutorial](https://spack-tutorial.readthedocs.io/en/latest/tutorial_modules.html).
+
+## Quick Start
+After cloning, you need to specify the corresponding module file root path information in `settings.ini` by modifying the content in `settings.ini.template`:
+
+```console
+cp settings.ini.template settings.ini
+vi settings.ini
+```
+
+To process the module files, just simply run the command below:
+
+```console
+python modulefilter.py
+```
+
+To generate the json template of packages for the later-editing work to specify the to-be-supported packages, just simply run the command:
+
+```console
+python gen_json.py
+```
+
+## File summary in the repository
+
+- The example module files in `tar` files:
+     -  `spack-modulefiles.tar.gz` contains the module files generated from Spack on UCLA Hoffman2 Cluster.
+     - `tacc-modulefiles.tar.gz` contains the module files in TACC as a reference.
+     -  `comet-modulefiles.tar.gz` contains the module files in Comet as a reference.
+- The python script `modulefilter.py` to import Spack-generated module files and generate the sqlite database and process/export to the new module path directory.
+    - It reads the configuration file `settings.ini` which is specific to every deployment. User should copy the `settings.ini.template` content to a file named `settings.ini` and modify the path information as needed.
+    - It will generate/re-create an sqlite database named as `apps.db` under the repo directory. 
+- The python script `gen_json.py` to generate the JSON file template from Spack-generated module files.
+    - `apps.jon` was created as an example from Hoffman2-based `apps.db`.
+    -  `apps.json.example` was an abbreviated version of `apps.json` for showing in the extended abstract and the full-sized paper.
+- Some misc files:
+     - `tcl2lua.tcl` is the script to convert TCL/C module file to Lua-based Lmod module file. 
+     - `notes` is for documenting the side-note discussion information.
+
--- modulefilter.py
+++ modulefilter.py
@@ -1,33 +1,144 @@
 #!/bin/python
 
 import os
+import subprocess
 from pprint import pprint
 import re
 from shutil import copyfile
+import json
+import tarfile
 
-modulepath_all = "/u/local/spack/spack/current/share/spack/modules/linux-centos6-x86_64"
-modulepath_new = "/u/local/spack/spack/current/share/spack/modules/bio"
+# Get the root path info from settings.ini
+import configparser
+settings = configparser.ConfigParser()
+settings._interpolation = configparser.ExtendedInterpolation()
+settings.read('settings.ini')
 
-filename = 'applist'  
-with open(filename) as f:  
-    applist = f.readlines()
-applist = [a.lower().strip() for a in applist]
+spackmodule_all = settings.get('ModulePATH', 'SpackModulePATH')
+modulepath_new = settings.get('ModulePATH', 'NewModulePATH')
 
-for app in applist:
-    dirlist = os.listdir(modulepath_all)
-    for d in dirlist:
-        x = re.search('(.+)-(\w.*)-(\w+)-(\d\.\d\.\d)-(\w+)$', d)
-        if x: 
-            appname = x.groups()[0]
-            appver  = x.groups()[1]
-            compname  = x.groups()[2]
-            compver  = x.groups()[3]
-            hashcode  = x.groups()[4]
-            if (appname!=app) : continue
-            modulefile_src = modulepath_all+'/'+d
-            modulefile_dst = modulepath_new+'/'+appname+'-'+appver+'-'+compname+'-'+compver
-            copyfile(modulefile_src, modulefile_dst)
-            print("Copy", appname, "done!")
+# Untar the spack module files generated from the already-installed spack packages
+spack_tar = tarfile.open('spack-modulefiles.tar.gz')
+spack_tar.extractall(spackmodule_all)
+spack_tar.close()
+
+tclpath_all = spackmodule_all+"/spackmodules/modules/linux-centos7-x86_64/"
+luapath_all = spackmodule_all+"/spackmodules/lmod/linux-centos7-x86_64/"
+
+# Recreate/reset the sqlite3 database
+import sqlite3
+conn = sqlite3.connect('apps.db')
+conn.row_factory = sqlite3.Row
+c = conn.cursor()
+c.execute('''DROP TABLE IF EXISTS apps''')
+c.execute('''CREATE TABLE apps (id INTEGER PRIMARY KEY AUTOINCREMENT, appname TEXT, appver TEXT, apphash TEXT, compname TEXT, compver TEXT, mpiname TEXT, mpiver TEXT, mpihash TEXT, hierpath TEXT, flatpath TEXT, custpath TEXT, present INTEGER DEFAULT 1)''')
+conn.commit()
+
+# json file stores app list to display to users
+filename = "apps.json"
+
+with open(filename, 'r') as jsonfile:
+    jsondata = json.load(jsonfile)
+
+def check(jsondata, appname, appver, compname='gcc', compver='4.8.5'):
+    if appname in jsondata.keys() :
+        ver = appver+'-'+compname+'-'+compver
+        if ver in jsondata[appname]['version'] :
+            #if ver == jsondata[appname]['default'] :
+            if jsondata[appname]['default'] in ver :
+                return 2
+            else:
+                return 1
+    return 0
+
+
+cmd = 'find '+luapath_all+' -type f'
+files = subprocess.check_output(cmd, shell=True)
+files = files.decode('utf-8')
+files = files.replace(luapath_all, '')
+filelist = files.splitlines()
+for f in filelist:
+    compName = compVer = mpiName = mpiVer = mpiHash = ''
+    path0, filename = os.path.split(f)
+    appVerName = os.path.splitext(filename)[0] # get the app's version name
+    appVerFull = appVerName.split('-')
+    appVer = appVerFull[0]
+    appHash = appVerFull[-1]
+    path1, appName = os.path.split(path0)      # get the app's name
+    path2, compVer = os.path.split(path1)
+    if compVer == 'Core':
+        compName = 'gcc'
+        compVer = '4.8.5'    
+    else:
+        path3, compName = os.path.split(path2)
+        if path3:
+            mpis = path3.split('/')
+            if 'mpi' in mpis[0]:
+                mpiName = mpis[0]
+                mpiVer, mpiHash = mpis[1].split('-')
+    c.execute("""INSERT INTO apps (appname, appver, apphash, compname, compver, mpiname, mpiver, mpihash, hierpath) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""", (appName, appVer, appHash, compName, compVer, mpiName, mpiVer, mpiHash, f))
+conn.commit()
+
+cmd = 'find '+tclpath_all+' -type f'
+files = subprocess.check_output(cmd, shell=True)
+files = files.decode('utf-8')
+files = files.replace(tclpath_all, '')
+filelist = files.splitlines()
+for f in filelist:
+    path0, appVerName = os.path.split(f)
+    appVerFull = appVerName.split('-')
+    appVer = appVerFull[0]
+    appHash = appVerFull[-1]
+    c.execute("SELECT * FROM apps WHERE apphash = '%s'" % appHash)
+    r = c.fetchone()
+    if r['compname'] == 'gcc' and r['compver'] == '4.8.5': # if app itself is a compiler or the app is compiled by system default gcc
+        custdir = os.path.join(modulepath_new, 'modulefiles', r['appname'])
+    else:
+        if r['mpiname']:
+            custdir = os.path.join(modulepath_new, r['compname'], r['compver'], r['mpiname'], r['mpiver'], 'modulefiles', r['appname'])
         else:
-            print("New pattern of module files found, need to update the regex search rule for this:",d)
+            custdir = os.path.join(modulepath_new, r['compname'], r['compver'], 'modulefiles', r['appname'])
 
+    showapp = check(jsondata, r['appName'], r['appver'], r['compname'], r['compver'])
+    if showapp :
+        os.makedirs(custdir, exist_ok=True)
+        dstpath = os.path.join(custdir, r['appver']) + '.lua'
+        srcpath = os.path.join(tclpath_all, f)
+        print( r['id'], r['appName'], r['appVer'], dstpath, srcpath )
+        #copyfile(srcpath, dstpath)
+        cmd = "./tcl2lua.tcl " + srcpath + " > " + dstpath
+        os.system(cmd)
+    
+        flatpath = srcpath.replace(tclpath_all, '')
+        custpath = dstpath.replace(modulepath_new, '')
+        c.execute("""UPDATE apps SET flatpath = ?, custpath = ? WHERE id = ?""", (flatpath, custpath, r['id']))
+        if showapp == 2: # set the default
+            cmd = 'echo set ModulesVersion '+'\\"'+r['appVer']+'\\" > '+custdir+'/.version'
+            os.system(cmd)
+    
+        # append the compiler or mpi directory to $MODULEPATH and add to family
+        if r['appname'] == r['compname'] :
+            newmodulepath = os.path.join(modulepath_new, r['compname'], r['appver'], 'modulefiles')
+            #appendmodulepath = 'prepend-path MODULEPATH "' + newmodulepath + '"'
+            appendmodulepath = 'prepend_path{"MODULEPATH","' + newmodulepath + '",delim=":",priority="0"}'
+            cmd = "sed -i -e '$a " + appendmodulepath + "' " + dstpath
+            os.system(cmd)
+            addfamily = 'family("compiler")'
+            cmd = "sed -i -e '$a " + addfamily + "' " + dstpath
+            os.system(cmd)
+    
+        c.execute("SELECT * FROM apps WHERE mpiname = '%s'" % r['appname'])
+        rmpi = c.fetchone()
+        if rmpi is not None :
+            newmodulepath = os.path.join(modulepath_new, r['compname'], r['compver'], rmpi['mpiname'], rmpi['mpiver'], 'modulefiles')
+            #appendmodulepath = 'prepend-path MODULEPATH "' + newmodulepath + '"'
+            appendmodulepath = 'prepend_path{"MODULEPATH","' + newmodulepath + '",delim=":",priority="0"}'
+            cmd = "sed -i -e '$a " + appendmodulepath + "' " + dstpath
+            os.system(cmd)
+            addfamily = 'family("mpi")'
+            cmd = "sed -i -e '$a " + addfamily + "' " + dstpath
+            os.system(cmd)
+
+conn.commit()
+conn.close()
+quit()
